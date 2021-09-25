dad286ce75b854a60306cb65b71507a145973651
Isabel Carrascal <47400746+micarrascal@users.noreply.github.com>
2021-09-25 15:14:07 +0000

    presence() updated

--- index.js
+++ index.js
@@ -2,17 +2,37 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-const mySecret = process.env['token']
+function presence(){
+  client.user.setPresence({
+      status: "online",
+      game: {
+          name: "DANNY | *help",
+          type: "Playing Birth Charts"
 
+      }
+  });
+}
+
+
+
+const mySecret = process.env['token']
+constconfig = require("./config.json")
 
 client.on("ready", () => {
     console.log("Estoy listo!");
+    presence();
  });
  
+ let predix = config.prefix;
  client.on("message", (message) => {
-   if(message.content.startsWith("ping")) {
-     message.channel.send("pong!");
-   }
+// avoid bucle 
+if(message.author.bot) return;
+if(!message.content.startsWith(prefix)) return;
+// commands  
+ const args= message.content.slice(prefix.length).trim().split(/ +/g);
+ const command = args.shift().toLowerCase();
+
+
  
  });
  
