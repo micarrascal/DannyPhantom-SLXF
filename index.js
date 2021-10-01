2aaa11130539ff7357e906e01d829dae1c04d9bb
Isabel Carrascal <47400746+micarrascal@users.noreply.github.com>
2021-10-01 23:04:47 +0000

    commands updated

--- index.js
+++ index.js
@@ -2,38 +2,41 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-function presence(){
+function presence() {
   client.user.setPresence({
-      status: "online",
-      game: {
-          name: "DANNY | *help",
-          type: "Playing Birth Charts"
+    status: "online",
+    game: {
+      name: "DANNY | *help",
+      type: "Playing Birth Charts"
 
-      }
+    }
   });
+}
+
+const mySecret = process.env['token']
+config = require("./config.json")
+
+client.on("ready", () => {
+  console.log("Estoy listo!");
+  presence();
+});
+
+let prefix = config.prefix;
+client.on("message", (message) => {
+  // avoid bucle 
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  // commands  
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+if(command === "horoscope")
+{
+  message.channel.send("a")
 }
 
 
 
-const mySecret = process.env['token']
-constconfig = require("./config.json")
+});
 
-client.on("ready", () => {
-    console.log("Estoy listo!");
-    presence();
- });
- 
- let predix = config.prefix;
- client.on("message", (message) => {
-// avoid bucle 
-if(message.author.bot) return;
-if(!message.content.startsWith(prefix)) return;
-// commands  
- const args= message.content.slice(prefix.length).trim().split(/ +/g);
- const command = args.shift().toLowerCase();
-
-
- 
- });
- 
- client.login(mySecret);
+client.login(mySecret);
