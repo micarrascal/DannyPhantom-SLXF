a21ceb40a6b5ec8f04ce865c9096823d892150e5
Cesar Quintero <ce.quintero@uniandes.edu.co>
2021-11-08 07:02:44 +0000

    version 1 funcional :3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const cartas = require('./cards.json')
 
 
 // When the client is ready, run this code (only once)
@@ -9,7 +10,6 @@ client.once('ready', () => {
 
 client.on('message', (message) =>{
   let prefix = 'pana'
-  console.log("PANAAA");
 
   if(message.author.bot) return;
   if(!message.content.startsWith(prefix)) return;
@@ -20,8 +20,24 @@ client.on('message', (message) =>{
 
 
   if (command === 'ping'){
-    console.log("Estoy listo!");
     message.channel.send('pong! operando con normalidad...')
+  }
+
+  if (command === 'test'){
+    message.channel.send('prueba de cartas...')
+    console.log(cartas.cards[1])
+  }
+
+  if (command === 'horoscope'){
+    message.channel.send('barajando cartas....')
+
+    cardId = Math.floor(Math.random() * (77));
+    console.log(cardId)
+
+    message.channel.send('se han alineado los astros.. \n\ntu carta es:', {files: ["./assets/images/deck/" + cartas.cards[cardId].img]})
+    setTimeout(()=>{
+        message.channel.send('descripcion de la carta: ' + cartas.cards[cardId].desc)
+    },500)
   }
 
 })
