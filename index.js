295a0836be56709d8a6c9f92253f4654e7e1a332
Cesar Quintero <ce.quintero@uniandes.edu.co>
2021-11-08 06:16:19 +0000

    script index modificado para nueva versión de node

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 
-function presence() {
-  client.user.setPresence({
-    status: "online",
-    game: {
-      name: "DANNY | *help",
-      type: "Playing Birth Charts"
-
-    }
-  });
-}
-
-const mySecret = process.env['token']
-config = require("./config.json")
-
-client.on("ready", () => {
-  console.log("Estoy listo!");
-  presence();
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Toy ready para el cumbion!');
 });
 
-let prefix = config.prefix;
-client.on("message", (message) => {
-  // avoid bucle 
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  // commands  
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+client.on('message', (message) =>{
+  let prefix = 'pana'
+  console.log("PANAAA");
+
+  if(message.author.bot) return;
+  if(!message.content.startsWith(prefix)) return;
+
+  let usuario = message.mentions.members.first()|| message.author
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase();
 
-if(command === "horoscope")
-{
-  message.channel.send("a")
-}
+
+  if (command === 'ping'){
+    console.log("Estoy listo!");
+    message.channel.send('pong! operando con normalidad...')
+  }
+
+})
 
 
-
-});
-
-client.login(mySecret);
+const mySecret = process.env['TOKEN']
+client.login(mySecret)
